024625b969e0e246cd5381258b4157ad111a8f6b
git-oykko <yt.onyxoykko@gmail.com>
2023-04-18 00:18:12 +0600

    forgot to update loadstring since structure changed

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/git-oykko/fyrines-hub-rblx/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/git-oykko/fyrines-hub-rblx/main/ui/loader.lua"))()
